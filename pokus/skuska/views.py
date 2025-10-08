e34f334d4bed532c5ebd9ad991a98cd38a1e8124
Matúš Paškala <mpaskala08@gmail.com>
2025-10-08 08:12:36 +0200

    skuska

---
+++ pokus/skuska/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponse
+                                            
+                                            
+def index(request):
+      return HttpResponse('Maťko!')
+
+# Create your views here.
