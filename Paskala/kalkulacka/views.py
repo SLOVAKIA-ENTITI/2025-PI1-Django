bb9c38cc5e415c3338a94ce7ef32c96ca9182b5a
Matúš Paškala <mpaskala08@gmail.com>
2025-10-15 09:02:49 +0200

    Kalkulacka final version

--- Paskala/kalkulacka/views.py
+++ Paskala/kalkulacka/views.py
@@ -4,15 +4,24 @@ def index(request):
     if request.method == "GET":
         vysledok = 0
     if request.method == "POST":
-        a = int(request.POST["a"])
-        b = int(request.POST["b"])
-        operator = request.POST["operator"]
-        if operator == "+":
-            vysledok = a + b
-        elif operator == "-":
-            vysledok = a - b
-        elif vysledok == "*":
-            vysledok = a * b
-        else:
-            vysledok = a / b 
+        try:
+            a = float(request.POST["a"])
+            b = float(request.POST["b"])
+            operator = request.POST["operator"]
+            if operator == "+":
+                vysledok = a + b
+            elif operator == "-":
+                vysledok = a - b
+            elif operator == "*":
+                vysledok = a * b
+            else:
+                vysledok = a / b
+        except ZeroDivisionError:
+            vysledok = "Nemozes delit nulov"
+        except ValueError:
+            vysledok = "Zadávaj len čísla"
+        except:
+            vysledok = "Chyba"
+
+
     return render(request, 'kalkulacka/index.html', dict(vysledok = vysledok) )
