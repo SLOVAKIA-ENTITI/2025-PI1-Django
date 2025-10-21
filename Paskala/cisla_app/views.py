849eb76d9373f23b47254d080b81a9a923204f87
Matúš Paškala <mpaskala08@gmail.com>
2025-10-21 18:53:28 +0200

    FIX PRVOCISEL

--- Paskala/cisla_app/views.py
+++ Paskala/cisla_app/views.py
@@ -1,38 +1,75 @@
 from django.shortcuts import render
 
 def index(request):
-    if request.method == "GET":
-        vysledok = 0
-        apar = " "
-        bpar = " "
-        aprv = " "
-        brpv = " "
+    # 🔹 Inicializácia premenných – budú existovať vždy
+    vysledok = ""
+    apar = ""
+    bpar = ""
+    aprv = ""
+    bprv = ""
+
     if request.method == "POST":
         try:
             a = float(request.POST["a"])
             b = float(request.POST["b"])
+
+            # Ktoré číslo je väčšie
             if a > b:
-                vysledok = a 
+                vysledok = a
             elif b > a:
                 vysledok = b
             else:
                 vysledok = "Čísla sa rovnajú"
 
+            # Párnosť
             if a % 2 == 0:
                 apar = "Číslo A je párne"
             else:
-                apar = "číslo A je nepárne"
+                apar = "Číslo A je nepárne"
 
             if b % 2 == 0:
                 bpar = "Číslo B je párne"
             else:
-                bpar = "číslo B je nepárne"
+                bpar = "Číslo B je nepárne"
 
-           
+            # Prvočíslo A
+            if a <= 1:
+                prvocisloa = False
+            else:
+                prvocisloa = True
+                for i in range(2, int(a)):
+                    if a % i == 0:
+                        prvocisloa = False
+                        break
 
+            if prvocisloa:
+                aprv = "Číslo A je prvočíslo."
+            else:
+                aprv = "Číslo A nie je prvočíslo."
 
-        except:
-            vysledok = "Chyba"
+            # Prvočíslo B
+            if b <= 1:
+                prvocislob = False
+            else:
+                prvocislob = True
+                for i in range(2, int(b)):
+                    if b % i == 0:
+                        prvocislob = False
+                        break
 
+            if prvocislob:
+                bprv = "Číslo B je prvočíslo."
+            else:
+                bprv = "Číslo B nie je prvočíslo."
 
-    return render(request, 'cisla_app/index.html', {"vysledok":vysledok, "apar":apar, "bpar":bpar, "aprv":aprv} )
+        except Exception as e:
+            vysledok = f"Chyba: {e}"
+
+    # 🔹 Render mimo try, aby sa vykonal vždy
+    return render(request, 'cisla_app/index.html', {
+        "vysledok": vysledok,
+        "apar": apar,
+        "bpar": bpar,
+        "aprv": aprv,
+        "bprv": bprv
+    })
