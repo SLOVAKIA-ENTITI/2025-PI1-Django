8fe360b819ff2779accc0450edd0be60480cc593
Matúš Paškala <146921257+SLOVAKIA-ENTITI@users.noreply.github.com>
2025-12-10 09:29:04 +0100

    Delete Paskala directory

--- Paskala/cisla_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CislaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cisla_app'
--- Paskala/skuska/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-                                            
-                                            
-def index(request):
-      return HttpResponse('Maťko!')
-
-# Create your views here.
